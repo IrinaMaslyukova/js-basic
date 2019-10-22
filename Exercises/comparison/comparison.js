a54ef39ce3313c52c2f18f7e07f1b1422a623d5a
IrinaMaslyukova <mogilat@ua.fm>
2019-10-22 21:05:18 +0300

    fix task according to comments

--- Exercises/comparison/comparison.js
+++ Exercises/comparison/comparison.js
@@ -1,6 +1,6 @@
 //1. Try comparison operators `>`, `<`, `===`, '<=', '>='  with operands of different types, and see results.
 const a = 4;
-const b = 5;
+const b = '5';
 if (a > b || a < b) {
   console.log('correct!');
 } else if (b === b && b <= b && b >= b) {
